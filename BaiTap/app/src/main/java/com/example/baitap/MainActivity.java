fdb41e2c7b38fdf4c0b2bdb269a8290968100664
unknown <1851010041Hang@ou.edu.vn>
2021-04-27 20:06:51 +0700

    Done Recycler View - Basic Layout

--- BaiTap/app/src/main/java/com/example/baitap/MainActivity.java
+++ BaiTap/app/src/main/java/com/example/baitap/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.baitap;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.example.baitap.adapter.DiscountClothesAdapter;
+import com.example.baitap.model.DiscountClothes;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    //declare whole variable
+    RecyclerView discountRecyclerView;
+    DiscountClothesAdapter discountClothesAdapter;
+    List<DiscountClothes> discountClothesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Adding data to model
+        discountClothesList = new ArrayList<>();
+        discountClothesList.add(new DiscountClothes(1,R.drawable.discountset));
+        discountClothesList.add(new DiscountClothes(2,R.drawable.discountdress));
+        discountClothesList.add(new DiscountClothes(3,R.drawable.discountjean));
+        discountClothesList.add(new DiscountClothes(4,R.drawable.discountmaxi));
+        discountClothesList.add(new DiscountClothes(5,R.drawable.discountsport));
+
+
+        //Reference
+        discountRecyclerView = findViewById(R.id.recycler_view_discount);
+
+        setDiscountRecycler(discountClothesList);
+    }
+
+    private void setDiscountRecycler(List<DiscountClothes> dataList) {
+        RecyclerView.LayoutManager layoutManager =  new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false);
+        discountRecyclerView.setLayoutManager(layoutManager);
+
+        discountClothesAdapter = new DiscountClothesAdapter(this,dataList);
+        discountRecyclerView.setAdapter(discountClothesAdapter);
     }
 }
---
+++ BaiTap/app/src/main/java/com/example/baitap/model/DiscountClothes.java
@@ -0,0 +1,28 @@
+package com.example.baitap.model;
+
+public class DiscountClothes {
+
+    Integer id;
+    Integer imageurl;
+
+    public DiscountClothes(Integer id, Integer imageurl) {
+        this.id = id;
+        this.imageurl = imageurl;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getImageurl() {
+        return imageurl;
+    }
+
+    public void setImageurl(Integer imageurl) {
+        this.imageurl = imageurl;
+    }
+}
